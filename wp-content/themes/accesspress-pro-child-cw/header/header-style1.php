70c6a95c7f9dcaeb3a90e0412385764b2e442549
DJ Wilkerson <djay.wilk@gmail.com>
2015-02-19 16:03:07 -0600

    Youth Styling and Social Adjustments
    Added the Youth section, logo, and styling.  Also branched off the
    header section from the parent theme to customize social links per
    section.

---
+++ wp-content/themes/accesspress-pro-child-cw/header/header-style1.php
@@ -0,0 +1,58 @@
+<?php 
+	global $accesspress_pro_options;
+	$accesspress_pro_settings = get_option( 'accesspress_pro_options', $accesspress_pro_options );
+	if(!empty($accesspress_pro_settings['sticky_header']) && $accesspress_pro_settings['sticky_header'] == 1){
+		$accesspress_pro_sticky_header = " sticky-header";
+	}else{
+		$accesspress_pro_sticky_header = "";
+	}
+?>
+    <div id="top-header">
+		<div class="ak-container">
+			<div class="site-branding">
+				
+				<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+				<?php if ( get_header_image() ) { ?>
+					<img src="<?php header_image(); ?>" alt="<?php bloginfo('name') ?>">
+				<?php } ?>
+				</a>
+				
+			</div><!-- .site-branding -->
+        
+
+			<div class="right-header clearfix">
+				<?php 
+				do_action( 'accesspress_pro_header_text' ); 
+                ?>
+                <div class="clear"></div>
+<!-- CUSTOM SOCIAL LINKS -->
+                
+        <div class="socials">
+				<a href="http://facebook.com/crosswalkworship" class="facebook" data-title="Facebook" target="_blank"><span class="font-icon-social-facebook"></span></a>
+                <a href="https://twitter.com/cwllumc" class="twitter" data-title="Twitter" target="_blank"><span class="font-icon-social-twitter"></span></a>
+                <a href="http://instagram.com/cwllumc" class="instagram" data-title="instagram" target="_blank"><span class="fa fa-instagram"></span></a>
+		
+		</div>
+                
+<!-- CUSTOME SOCIAL LINKS -->
+                <?php
+				if($accesspress_pro_settings['show_search'] == 1){ ?>
+				<div class="ak-search">
+					<?php get_search_form(); ?>
+				</div>
+				<?php } ?>
+			</div><!-- .right-header -->
+		</div><!-- .ak-container -->
+ 	</div><!-- #top-header -->
+
+  	<nav id="site-navigation" class="main-navigation <?php echo $accesspress_pro_sticky_header; ?>">
+		<div class="ak-container">
+			<h1 class="menu-toggle"><?php _e( 'Menu', 'accesspress_pro' ); ?></h1>
+			<?php wp_nav_menu( array( 
+					'theme_location' => 'primary',
+					'container'      => 'div',
+					'container_class'=> 'menu',
+					'items_wrap'      => '<ul>%3$s</ul>',
+					 ) ); ?>
+		</div>
+	</nav><!-- #site-navigation -->
